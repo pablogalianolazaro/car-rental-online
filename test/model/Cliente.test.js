39bb730ed1005fe48b1685dd4b60aba086d0810e
Lucia Montanana <lumontanana@gmail.com>
2023-11-03 13:20:45 +0100

    cambios test

--- test/model/Cliente.test.js
+++ test/model/Cliente.test.js
@@ -12,7 +12,7 @@ describe('Cliente', () => {
 
   it('Setter y Getter para DNI', () => {
     const cliente = new Cliente(1);
-    cliente.dni = '12345678';
+    cliente.dni('12345678');
     expect(cliente.dni).to.equal('12345678');
   });
 
@@ -20,6 +20,58 @@ describe('Cliente', () => {
 
   it('Rol debería ser "Cliente"', () => {
     const cliente = new Cliente(1);
-    expect(cliente.rol).to.equal('Cliente');
+    expect(cliente.rol).to.equal(rol.Cliente);
+  });
+  it('Constructor debería inicializar las propiedades _id y _rol', () => {
+    const cliente = new Empleado(1);
+    expect(cliente._id).to.equal(1);
+    expect(cliente._rol).to.equal('Empleado');
+  });
+
+  it('Setter y Getter para DNI', () => {
+    const cliente = new Empleado(2);
+    cliente.dni('87654321')
+    expect(cliente.dni).to.equal('87654321');
+  });
+
+  it('Setter y Getter para Nombres', () => {
+    const cliente = new Cliente(3);
+    cliente.nombres('Ana')
+    expect(cliente.nombres).to.equal('Ana');
+  });
+
+  it('Setter y Getter para Apellidos', () => {
+    const cliente = new Cliente(4);
+    cliente.apellidos('González');
+    expect(cliente.apellidos).to.equal('González');
+  });
+
+  it('Setter y Getter para Dirección', () => {
+    const cliente = new Cliente(5);
+    cliente.direccion('Calle 456');
+    expect(cliente.direccion).to.equal('Calle 456');
+  });
+
+  it('Setter y Getter para Email', () => {
+    const cliente = new cliente(6);
+    cliente.email('ana@example.com');
+    expect(cliente.email).to.equal('ana@example.com');
+  });
+
+  it('Setter y Getter para Password', () => {
+    const cliente = new Cliente(7);
+    cliente.password('pass123');
+    expect(cliente.password).to.equal('pass123');
+  });
+
+  it('Setter y Getter para Teléfono', () => {
+    const cliente = new Cliente(8);
+    cliente.telefono('987654321');
+    expect(cliente.telefono).to.equal('987654321');
+  });
+
+  it('Rol debería ser "Empleado"', () => {
+    const cliente = new Empleado(9);
+    expect(cliente.rol).to.equal(rol.cliente);
   });
 });
--- test/model/Empleado.test.js
+++ test/model/Empleado.test.js
@@ -11,43 +11,43 @@ describe('Empleado', () => {
 
   it('Setter y Getter para DNI', () => {
     const empleado = new Empleado(2);
-    empleado.dni = '87654321';
+    empleado.dni('87654321')
     expect(empleado.dni).to.equal('87654321');
   });
 
   it('Setter y Getter para Nombres', () => {
     const empleado = new Empleado(3);
-    empleado.nombres = 'Ana';
+    empleado.nombres('Ana')
     expect(empleado.nombres).to.equal('Ana');
   });
 
   it('Setter y Getter para Apellidos', () => {
     const empleado = new Empleado(4);
-    empleado.apellidos = 'González';
+    empleado.apellidos('González');
     expect(empleado.apellidos).to.equal('González');
   });
 
   it('Setter y Getter para Dirección', () => {
     const empleado = new Empleado(5);
-    empleado.direccion = 'Calle 456';
+    empleado.direccion('Calle 456');
     expect(empleado.direccion).to.equal('Calle 456');
   });
 
   it('Setter y Getter para Email', () => {
     const empleado = new Empleado(6);
-    empleado.email = 'ana@example.com';
+    empleado.email('ana@example.com');
     expect(empleado.email).to.equal('ana@example.com');
   });
 
   it('Setter y Getter para Password', () => {
     const empleado = new Empleado(7);
-    empleado.password = 'pass123';
+    empleado.password('pass123');
     expect(empleado.password).to.equal('pass123');
   });
 
   it('Setter y Getter para Teléfono', () => {
     const empleado = new Empleado(8);
-    empleado.telefono = '987654321';
+    empleado.telefono('987654321');
     expect(empleado.telefono).to.equal('987654321');
   });
 
