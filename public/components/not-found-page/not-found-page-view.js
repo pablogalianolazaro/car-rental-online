89aa6ca791cf984dbdbd57d450e6df9089dcb57d
Pablo Galiano <pablo.galiano2@alu.uclm.es>
2023-11-21 20:50:25 +0100

    Desarrollo de la práctica 2.2.

--- public/components/not-found-page/not-found-page-view.js
+++ public/components/not-found-page/not-found-page-view.js
@@ -1,10 +1,4 @@
 class NotFoundPageView extends PageView {
-    url;
     constructor() { super('not-found-page'); }
-    // Vista
-    get urlParagraph() { return document.getElementById('url'); }
-    async refresh(url) {
-        await super.refresh(url);
-        this.urlParagraph.innerHTML = this.url;
-    }
+    async refresh(url) { await super.refresh(url); }
 }
