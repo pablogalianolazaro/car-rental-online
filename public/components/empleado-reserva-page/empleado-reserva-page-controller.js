68dcbedb9c1edd7206cc078567473001c7f0709e
Alba <albaggibiza@gmail.com>
2023-11-26 04:36:39 +0100

    parte de empleado-reserva-page

--- public/components/empleado-reserva-page/empleado-reserva-page-controller.js
+++ public/components/empleado-reserva-page/empleado-reserva-page-controller.js
@@ -4,16 +4,35 @@ class EmpleadoReservaPageController extends PageController {
         this.view = new EmpleadoReservaPageView();
     }
 
-    async cargarMostrarReserva(reservaId) {
-        try {
-            // Obtener la reserva por su ID
-            const reserva = await obtenerReservaPorId(reservaId);
+    async refresh(url) {
+        await super.refresh(url);
 
-            // Mostrar la reserva en la vista
-            this.view.mostrarReserva(reserva);
-        } catch (error) {
-            console.error('Error al cargar y mostrar la reserva:', error);
-        }
+        // Obtener parámetros de la URL
+        const urlParams = new URLSearchParams(window.location.search);
+        const reservaNum = urlParams.get('numReserva');
+        const reservaId = urlParams.get('reservaId');
+
+        // Obtener la reserva por su número
+        const reserva = getReservaByNumero(reservaNum);
+
+        // Obtener cliente y vehículo asociados a la reserva
+        const cliente = this.model.clienteById(reserva.clienteId);
+        const vehiculo = this.model.vehiculoById(reserva.vehiculoId);
+
+        // Mostrar la información en la vista
+        this.view.setReserva(reserva, cliente, vehiculo);
+    }
+
+    async refresh(url) {
+        await super.refresh(url);
+        const url1 = new URL('/car-rental-online/empleado-reserva-page?numReserva=$12353&reservaId=$20');
+        //Obtenemos reserva
+        var reservaNum = getParameterByName('numReserva');
+        let reserva = getReservaByNumero(reservaNum);
+        let cliente = this.model.clienteById(reserva.clienteId);
+        let vehiculo = this.model.vehiculoById(reserva.vehiculoId);
+        this.view.setReserva(reserva1, cliente, vehiculo);
+        
     }
 
     async signout(event){
@@ -21,26 +40,35 @@ class EmpleadoReservaPageController extends PageController {
         event.target.href = "car-rental-online/invitado-home-page";
         router.route(event);
     }
-}
 
-//esto no se como hacerlo
-const reservasLocales = [
-    { _id: '1', matricula: 'ABC123', fechaInicio: '2023-01-01', fechaFin: '2023-01-05' },
-    { _id: '2', matricula: 'XYZ789', fechaInicio: '2023-02-01', fechaFin: '2023-02-10' },
+    async entregarVehiculo() {
+        try {
+            // Llamada al modelo para entregar el vehículo
+            await this.model.entregarVehiculo(this.reservaId);
 
-];
-
-async function obtenerReservaPorId(reservaId) {
-    try {
-        const reservaEncontrada = reservasLocales.find(reserva => reserva._id === reservaId);
-
-        if (reservaEncontrada) {
-            return Promise.resolve(reservaEncontrada);
-        } else {
-            return Promise.reject(new Error(`Reserva con ID ${reservaId} no encontrada`));
+            // Navegar a la página de listado de reservas para empleados
+            navegarAListaReservas();
+        } catch (error) {
+            console.error('Error al entregar el vehículo:', error);
         }
-    } catch (error) {
-        return Promise.reject(error);
+    }
+
+    async devolverVehiculo() {
+        try {
+            // Llamada al modelo para devolver el vehículo (sincrónica)
+            modeloReservas.devolverVehiculo();
+
+            // Navegar a la página de listado de reservas
+            navegarAListaReservas();
+        } catch (error) {
+            console.error('Error al devolver el vehículo:', error);
+        }
+    }
+
+    async navegarAListaReservas() {
+        console.log('Redirigiendo a la página de listado de reservas para empleados');
+        window.location.href = 'empleado-reservas-page.html';
     }
 }
+
 
